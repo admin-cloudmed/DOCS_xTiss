74dfbb0b9469486dcfd4100e7a4ff6b199582a36
unknown <lander@cloudmed.io>
2014-03-17 11:11:35 -0300

    Ajuste em especicificacao das validacoes e descricao de erros

---
+++ public/img/downloads/EspecificacaodasValidacoes_files/wz_jsgraphics.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /preview/7b8/7b8dabda-addd-11e3-a493-002590d31986/7b8dabda-addd-11e3-a493-002590d31986_id_EspecificacaodasValidacoes_files/wz_jsgraphics.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.15 (CentOS) Server at www.pdf.investintech.com Port 80</address>
+</body></html>
---
+++ public/img/downloads/descricaoErrosXtiss_files/wz_jsgraphics.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /preview/94e/94e16802-addc-11e3-a493-002590d31986/94e16802-addc-11e3-a493-002590d31986_id_descricaoErrosXtiss_files/wz_jsgraphics.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.15 (CentOS) Server at www.pdf.investintech.com Port 80</address>
+</body></html>
